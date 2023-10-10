b8ef105ee86381c1026a9d28c7fa6be8bcb83c57
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-10-10 10:23:01 +0200

    pokemonBulbiOk

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,33 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
 function PokemonCard() {
+
+    const pokemon = pokemonList[0];
+
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon image" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
+        <div>
+            <figure>
+                <figcaption>{pokemon.name}</figcaption>
+                {pokemon.imgSrc ? (
+                    <img src={pokemon.imgSrc} alt="image pokemon" />
+                ) : (
+                    <p>????</p>
+                )}
+            </figure>
+        </div>
 
     );
-
 }
 
 export default PokemonCard;
+
